5c23ebb848a4f13a0d37ae1e498c5a1261a12147
katherina17 <katherinepetrova76@gmail.com>
2022-02-21 23:29:44 +0300

    init: created a function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable (number){
+    let result = "";
+    if(number == 0) return "zero";
+    let digit = ['', 'one','two','three','four', 'five','six','seven','eight','nine'];
+    let tens = ['', 'ten', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+    let special = ['ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen', 'seventeen','eighteen','nineteen'];
+    let lastTwo = number % 100;
+    if( lastTwo >= 10 && lastTwo <=19){
+        result = special[lastTwo - 10];
+    } else{
+        let lastDigit = lastTwo % 10;
+        let secondDigit = Math.floor(lastTwo / 10);
+        result = tens[secondDigit] + (secondDigit == 0 || lastDigit == 0 ? "" : " ") + digit[lastDigit];
+    }
+    if(number >= 100){
+        let firstDigit = Math.floor(number / 100);
+        result = digit[firstDigit] + " " + "hundred" + (lastTwo == 0 ? "" : " ") + result;
+    }
+
+    return result;
 }
